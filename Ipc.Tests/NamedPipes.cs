21ae04eff50a85103d2a5925231641b8f20ce474
Wazzq <wuhqureshi@gmail.com>
2013-03-19 15:07:02 +0000

    Example showing the NamedPipeServerStream working in a test. Also does a User Agent string search against FiftyOne.Interop to return if it's Mobile

---
+++ Ipc.Tests/NamedPipes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Ipc.Tests
+{
+	[TestFixture]
+	public class NamedPipes
+	{
+		[TestCase("Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; da-dk) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5", "True")]
+		[TestCase("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1", "False")]
+		public void NamedPipeServerDetectAgent(string agent, string expectedResult)
+		{
+			var pipeServer = new PipeServer();
+
+			var serverThread = new Thread(pipeServer.Start);
+			serverThread.Start();
+
+			string response = "";
+
+			using (var pipeStream = new NamedPipeClientStream(".", "testpipe", PipeDirection.InOut))
+			using (var streamReader = new StreamReader(pipeStream))
+			using (var streamWriter = new StreamWriter(pipeStream))
+			{
+				pipeStream.Connect();
+
+				if (pipeStream.IsConnected)
+				{
+					streamWriter.AutoFlush = true;
+					streamWriter.WriteLine(agent);
+
+					response = streamReader.ReadLine();
+				}
+				else
+				{
+					Assert.Fail("pipe was not connected");
+				}
+			}
+
+			Assert.AreEqual(expectedResult, response);
+		}
+	}
+}
---
+++ Ipc.Tests/PipeServer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FiftyOne.Mobile.Detection.Provider.Interop;
+
+namespace Ipc.Tests
+{
+	public class PipeServer
+	{
+		public void Start()
+		{
+			//Create a named pipe
+			using (var pipeStream = new NamedPipeServerStream("testpipe", PipeDirection.InOut, 50))
+			using (var streamReader = new StreamReader(pipeStream))
+			using (var streamWriter = new StreamWriter(pipeStream))
+			{
+				//wait for a connection from another process
+				pipeStream.WaitForConnection();
+				Console.WriteLine("[Server] Pipe connection established");
+				{
+					string userAgent;
+					//wait for message to arrive from the pipe
+					while ((userAgent = streamReader.ReadLine()) != null)
+					{
+						Console.WriteLine("[Server Rcvd User Agent] {0}: {1}", DateTime.Now, userAgent);
+
+						var isMobile = IsMobilePattern(userAgent);
+
+						streamWriter.AutoFlush = true;
+						streamWriter.WriteLine(isMobile);
+						Console.WriteLine("[Server] sent message back to client IsMobile:{0}", isMobile);
+					}
+				}
+			}
+			Console.WriteLine("Connection lost");
+		}
+
+		private static bool IsMobileTrie(string userAgent)
+		{
+			var isMobile = false;
+
+			var trieWrapper = new TrieWrapper(@"C:\websites\online\DeviceRedirectionData\51Degrees.mobi-Lite-2013.01.02.trie.dat");
+
+			var properties = trieWrapper.GetProperties(userAgent);
+			var isMobilePropertyList = properties["IsMobile"];
+			bool.TryParse(isMobilePropertyList.First(), out isMobile);
+			return isMobile;
+		}
+
+		private static bool IsMobilePattern(string userAgent)
+		{
+			var isMobile = false;
+
+			var wrapper = new PatternWrapper(new[] { "IsMobile" });
+
+			var properties = wrapper.GetProperties(userAgent);
+			var isMobilePropertyList = properties["IsMobile"];
+			bool.TryParse(isMobilePropertyList.First(), out isMobile);
+			return isMobile;
+		}
+	}
+}
